feef0d3831cdecd89c56d84d4335d5e83e116912
Juan Quiroz <juandavidlopezquiroz@gmail.com>
2023-05-15 13:06:00 -0500

    primera sin cambios

---
+++ app.js
@@ -0,0 +1,80 @@
+//console.log(`hola mundo`);
+
+/*function hello () {
+    console.log (`hola mundo`);
+}
+hello();*/
+
+/*function hello () {
+    console.log (`hola mundo`);
+    console.log(`trabajndo en JS`);
+}
+hello()*/
+
+/*function hello () {
+    return `hola mundo`;
+}
+
+hello()*/
+
+/*function hello () {
+    return `hola mundo`;
+}
+
+const result = hello()
+console.log(re)*/
+
+/*function hello () {
+    return `hola mundo`;
+}
+console.log(hello())*/
+
+/*function hello () {
+    return function (){
+        return `hola mundo`*/
+
+/*const user = {
+    name: 'juan',
+    lastname: 'lopez',
+    age: 18,
+    address: {
+        country: 'colombia',
+        city: 'armenia',
+        street: 'N 12' 
+    },
+    friends: ['Andres', 'Cristian'], 
+    active: true, 
+    sendMail() {
+        return 'sending email...';
+    },
+};
+
+console.log(user.address.city);
+console.log(user.friends);
+console.log(user.active);
+console.log(user.sendMail());*/
+
+/*const name = 'laptop'
+const price = 3000
+
+const newProduct = {
+    name,
+    price
+}
+console.log(newProduct)*/
+
+/*const title = document.createElement('h1')
+title.innerText = 'Hola mundo desde js'
+
+const button = document.createElement
+('button')
+button.innerText = 'click'
+
+button.addEventListener('click', function(){
+    title.innerText = 'Texto actualizado con JS'
+    alert('se realizo el click')
+})
+
+document.body.append(title)
+document.body.append(button)*/
+
